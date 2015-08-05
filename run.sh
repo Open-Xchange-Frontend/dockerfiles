666a6fbf9a99a36e9df4764ebdf92ee69d582a11
Julian Bäume <julian.baeume@open-xchange.com>
2015-08-05 19:38:58 +0200

    deactivate bower user survey spyware
    we generally can not assure user interaction with a tty is possible, so
    we deactivate it by default

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+mkdir -p /home/ox/.config/configstore && echo "optOut: true" > /home/ox/.config/configstore/insight-bower.yml
+
 rm bower_components/ node_modules/ -rf
 
 npm i --production
